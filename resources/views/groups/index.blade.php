1e89e0d07d0532b7d75dfcf5a8ea2a09c12b3d40
mrifkysaputra18 <mrifkysaputramc@gmail.com>
2025-10-13 21:36:17 +0800

    otomatis select kelas sesuai yang di filter saat tambah kelompok

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -17,7 +17,7 @@
                     <i class="fas fa-file-excel mr-2"></i>Import Excel
                 </a>
                 @endif
-                <a href="{{ route('groups.create') }}" 
+                <a href="{{ route('groups.create', ['classroom' => request('classroom')]) }}" 
                    class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
                     <i class="fas fa-plus mr-2"></i>Tambah Kelompok
                 </a>
@@ -238,7 +238,7 @@
                             </div>
                             <h3 class="text-lg font-medium text-gray-900 mb-2">Belum ada kelompok</h3>
                             <p class="text-gray-500 mb-4">Mulai dengan menambahkan kelompok pertama Anda.</p>
-                            <a href="{{ route('groups.create') }}" 
+                            <a href="{{ route('groups.create', ['classroom' => request('classroom')]) }}" 
                                class="inline-flex items-center px-4 py-2 bg-primary-500 hover:bg-primary-700 text-white font-bold rounded-lg shadow-md transition duration-300">
                                 <i class="fas fa-plus mr-2"></i>Tambah Kelompok Pertama
                             </a>
