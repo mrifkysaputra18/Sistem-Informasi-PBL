d4e11a1bc7b7eed2076c3ae9d88fe5e2aeab10b2
Muhammad Eka yusda putra <mhmmadputra95@gmail.com>
2025-10-14 17:14:01 +0800

    hapus data dummy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AcademicTermSeeder::class,
-            SubjectSeeder::class,
-            CriterionSeeder::class,
-            UserSeeder::class,
-            ClassRoomSeeder::class,
-            CompletePBLDataSeeder::class,
+            // Data Master (PENTING - Jangan dihapus)
+            AcademicTermSeeder::class,      // Periode akademik
+            SubjectSeeder::class,            // Mata kuliah
+            CriterionSeeder::class,          // Kriteria penilaian
+            ClassRoomSeeder::class,          // Kelas TI-3A sampai TI-3E
+            DosenSeeder::class,              // Dosen real Politala
+            
+            // ❌ DUMMY SEEDER - DINONAKTIFKAN
+            // UserSeeder::class,            // Mahasiswa dummy 1-10 + admin dummy
+            // StudentSeeder::class,         // Generate 25 mahasiswa dummy per kelas
+            // GroupSeeder::class,           // Generate kelompok dummy
+            // CompletePBLDataSeeder::class, // Generate 125 mahasiswa + kelompok dummy
         ]);
     }
 }
